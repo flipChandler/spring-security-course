50ef2b4fc891f3540d51e70529137eef54a133a6
flipChandler <felipe.santos11@hotmail.com>
2022-02-04 09:30:47 -0300

    BCryptPasswordEncoder setup

--- src/main/java/com/amigoscode/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/amigoscode/springsecurity/security/SecurityConfig.java
@@ -1,13 +1,25 @@
 package com.amigoscode.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
    /* // qlq request tem que estar autenticado pelo httpBasic
     // no way to logout
@@ -30,5 +42,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    // to retrieve user from the database
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")           // spring interpreta como ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
