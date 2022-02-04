76e4610d3fe8dff19dc4af1d05d114914f5c9634
flipChandler <felipe.santos11@hotmail.com>
2022-02-03 22:05:23 -0300

    http basic authentication

---
+++ src/main/java/com/amigoscode/springsecurity/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.amigoscode.springsecurity.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+   /* // qlq request tem que estar autenticado pelo httpBasic
+    // no way to logout
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }*/
+
+    // raiz, index, com css e js são white lists, o resto tem que estar autenticado
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
