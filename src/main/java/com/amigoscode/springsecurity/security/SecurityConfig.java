1e9b9c2100e3b73bcfb00b6a9850d4c2e1a10eb2
flipChandler <felipe.santos11@hotmail.com>
2022-02-07 18:06:18 -0300

    form based authentication

--- src/main/java/com/amigoscode/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/amigoscode/springsecurity/security/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.amigoscode.springsecurity.security.UserRole.*;
 
@@ -47,22 +50,51 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }*/
 
-    // a ordem dos antMatchers importa
+    /* a ordem dos antMatchers importa - httpBasic
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable()                                                   // cross site request forgery     || when disable, spring security is not validating the csrf token given in login
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
-//                .antMatchers(DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    } */
+
+    // form login - by default a session id last 30 minutes
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()                                                   // cross site request forgery     || when disable, spring security is not validating the csrf token given in login
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")          // default 2 weeks | customize to 21 days
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))          // if csrf is enabled, delete this line
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     // to retrieve user from the database
